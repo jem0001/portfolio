186892f61057e7f8ac1a7692593921529c1c1825
Jemuel <salcedojemuel25@gmail.com>
2024-02-12 17:41:24 +0800

    first-commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        secondary: "#0a101e;",
+        primary: "#0A1033",
+        accent: "#fec544",
+      },
+    },
+  },
+  plugins: [],
+};
